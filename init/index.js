37f3deeade180064c87af68b688b7604dc7f6f29
Naman Patidar <namanpatidar800@gmail.com>
2026-07-04 15:52:34 +0530

    update

--- init/index.js
+++ init/index.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+require("dotenv").config({ path: "../.env" });
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 main()
     .then(() =>
@@ -14,12 +18,24 @@ async function main()
   await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
-const initDB = async() =>
+const initDB = async () =>
 {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner : "6a33b9e44dfe4e3cb644b774"}));
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
+    const listings = [];
+    for (let obj of initData.data) 
+    {
+        let response = await geocodingClient
+            .forwardGeocode({
+                query: `${obj.location}, ${obj.country}`,
+                limit: 1,
+            })
+            .send();
+        obj.owner = "6a33b9e44dfe4e3cb644b774";
+        obj.geometry = response.body.features[0].geometry;
+        listings.push(obj);
+    }
+    await Listing.insertMany(listings);
+    console.log("Data was initialized");
 };
 
 initDB();

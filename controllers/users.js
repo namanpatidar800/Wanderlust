bad4897a551972bc5fa3daff30a9521594803076
Naman Patidar <namanpatidar800@gmail.com>
2026-07-09 21:53:10 +0530

    Add Project Files

--- controllers/users.js
+++ controllers/users.js
@@ -12,7 +12,6 @@ module.exports.signup = async (req, res, next) =>
         let {username, email, password} = req.body;
         const newUser = new User({email, username});
         const registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
         req.login(registeredUser, (err) =>
         {
             if(err)

1229e8c460d4c33f68825bc0ab4feffd7f36637d
Naman Patidar <namanpatidar800@gmail.com>
2026-07-09 16:40:58 +0530

    Add Project Files

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const dbUrl = process.env.ATLASDB_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const dbUrl = process.env.ATLASDB_URL;
 
 main()
     .then(() =>
@@ -36,7 +36,7 @@ main()
 
 async function main()
 {
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(MONGO_URL);
 };
 
 app.set("view engine", "ejs");
@@ -48,7 +48,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create(
     {
-        mongoUrl : dbUrl,
+        mongoUrl : MONGO_URL,
         crypto : {
             secret : process.env.SECRET,
         },
